ab6a7610e93977bb3e1742b38c87da4d737f9d87
Mindhunter26 <55491377+Mindhunter26@users.noreply.github.com>
2019-10-13 17:45:36 +0300

    Create code.cpp

---
+++ Lab2/code.cpp
@@ -0,0 +1,43 @@
+#include "pch.h"
+#include <iostream>
+#include <limits>
+#include <math.h>
+#include <cmath>
+
+#define Infinity std::numeric_limits<double>::infinity()
+using namespace std;
+
+double a, b;
+
+double f(double x) {
+	if (x <= a) {
+		return log(x);
+	}
+	else if ((a < x) && (x < b)) {
+		return 1;
+	}
+	else if (x >= b) {
+		return exp(x);
+	}
+}
+
+
+int main() {
+	setlocale(LC_ALL, "Rus");
+	double x1, x2, dx;
+	cout << "Введите a, b, xнач, xкон, dx: ";
+	cin >> a >> b >> x1 >> x2 >> dx;
+
+	double max = -Infinity, min = Infinity;
+	for (double x = x1; x <= (x2+dx); x += dx) {
+		double value = f(x);
+		if (value < min) min = value;
+		if (value > max) max = value;
+
+		cout << "f(" << x << ") = " << value << endl;
+	}
+	cout << "---------------" << endl;
+	cout << "min= " << min << endl << "max= " << max << endl;
+
+	return 0;
+}
