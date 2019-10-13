61a1c98abd6486f3434158e34bbefa5bb5a3d484
Mindhunter26 <55491377+Mindhunter26@users.noreply.github.com>
2019-10-13 17:50:26 +0300

    Update code.cpp

--- Lab2/code.cpp
+++ Lab2/code.cpp
@@ -3,12 +3,9 @@
 #include <limits>
 #include <math.h>
 #include <cmath>
-
 #define Infinity std::numeric_limits<double>::infinity()
 using namespace std;
-
 double a, b;
-
 double f(double x) {
 	if (x <= a) {
 		return log(x);
@@ -21,18 +18,19 @@ double f(double x) {
 	}
 }
 
-
 int main() {
 	setlocale(LC_ALL, "Rus");
 	double x1, x2, dx;
 	cout << "Введите a, b, xнач, xкон, dx: ";
 	cin >> a >> b >> x1 >> x2 >> dx;
 
-	double max = -Infinity, min = Infinity;
+	double max = -Infinity, min = Infinity;//Несмотря на красную линию , программа работает верно
 	for (double x = x1; x <= (x2+dx); x += dx) {
 		double value = f(x);
-		if (value < min) min = value;
-		if (value > max) max = value;
+		if (value < min)
+			min = value;
+		if (value > max) 
+			max = value;
 
 		cout << "f(" << x << ") = " << value << endl;
 	}
@@ -41,3 +39,4 @@ int main() {
 
 	return 0;
 }
+
